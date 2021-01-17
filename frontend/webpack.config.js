fd30eb9244325edcf564a127bbb2c3d372d01842
Филатов Алексей Алексеевич <Filatov.A.Alekseev@sberbank.ru>
2021-01-17 19:20:42 +0300

    added app for front

---
+++ frontend/webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const settings = {
+    distPath: path.join(__dirname, 'dist'),
+    srcPath: path.join(__dirname, 'src')
+};
+
+function srcPathExtend(subpath) {
+    return path.join(settings.srcPath, subpath);
+}
+
+const isDevMode = process.env.NODE_ENV === 'development';
+
+module.exports = (/* env, options */) => ({
+    entry: settings.srcPath,
+    output: {
+        path: settings.distPath,
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    devtool: isDevMode ? 'source-map' : false,
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            // {
+            //     enforce: 'pre',
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: 'eslint-loader'
+            // },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s?[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer()
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets/fonts'
+                    }
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|ico)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/images'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: srcPathExtend('index.html')
+        }),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
+});
